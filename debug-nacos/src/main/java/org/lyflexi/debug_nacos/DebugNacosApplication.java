48dd9570005f9cfa793f8a3a20d3ebad968d1d92
lyremote <liuyanoutsee@outlook.com>
2024-09-26 13:34:41 +0800

    [a]提前干预nacos服务的注册行为

---
+++ debug-nacos/src/main/java/org/lyflexi/debug_nacos/DebugNacosApplication.java
@@ -0,0 +1,13 @@
+package org.lyflexi.debug_nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebugNacosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebugNacosApplication.class, args);
+	}
+
+}
---
+++ debug-nacos/src/test/java/org/lyflexi/debug_nacos/DebugNacosApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lyflexi.debug_nacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DebugNacosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
